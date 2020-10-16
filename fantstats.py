d6156eda71e7e4951e8cb237643fb0aff1c67484
Matthew Mercuri <mercurimatthew@gmail.com>
2020-10-16 00:36:26 -0400

    added skew and moved main function to top

--- fantstats.py
+++ fantstats.py
@@ -4,15 +4,6 @@ import pandas as pd
 import time
 
 ''' what to do:
-- pull active players names
-- sort by position
-- fetch stats
-
-what stats do we want:
-- median fantpts
-- std of fantpts
-- 1 SD interval
-
 - perhaps efficiency metrics
     - yards per carry
 '''
@@ -29,6 +20,91 @@ RUSHING_YARDS = 0.1
 RUSHING_TD = 6
 RECEIVING_YARDS = 0.1
 RECEIVING_TD = 6
+
+
+def main():
+    players_dict = Data.players()
+    fant_df = Data.fantasy_df()
+
+    _check_if_in_fant_df(players_dict, fant_df)
+
+    positions_dict = _get_positions(players_dict, fant_df)
+
+    players_dict, dropped = _drop_no_pos_players(players_dict, positions_dict)
+
+    pos_df = pd.DataFrame.from_dict(positions_dict, orient='index')
+
+    main_df = pd.DataFrame.from_dict(players_dict, orient='index')
+
+    main_df = pd.concat([main_df, pos_df], axis=1)
+    main_df.dropna(inplace=True)
+    main_df.columns = ['Link', 'Pos']
+
+    qbs = main_df[main_df['Pos'] == 'QB'].index.tolist()
+    rbs = main_df[main_df['Pos'] == 'RB'].index.tolist()
+    wrs = main_df[main_df['Pos'] == 'WR'].index.tolist()
+
+    # ========== QB ==========
+    qb_dfs = []
+    tot_qbs = len(qbs)
+    i = 0
+    for qb in qbs:
+        qb_df = get_player_df(qb)
+        if len(qb_df) >= MIN_GAMES:
+            try:
+                final_df = _qb(qb, qb_df)
+                time.sleep(2)
+                qb_dfs.append(final_df)
+            except Exception:
+                pass
+
+        i += 1
+        print(f'{round(i/tot_qbs, 2)*100}% of QBs complete')
+
+    qb_df = pd.concat(qb_dfs)
+    qb_df.to_csv('results/qbs.csv')
+    # ========================
+
+    # ========== RB ==========
+    rb_dfs = []
+    tot_rbs = len(rbs)
+    i = 0
+    for rb in rbs:
+        rb_df = get_player_df(rb)
+        if len(rb_df) >= MIN_GAMES:
+            try:
+                final_df = _rb(rb, rb_df)
+                time.sleep(2)
+                rb_dfs.append(final_df)
+            except Exception:
+                pass
+
+        i += 1
+        print(f'{round(i/tot_rbs, 2)*100}% of RBs complete')
+
+    rb_df = pd.concat(rb_dfs)
+    rb_df.to_csv('results/rbs.csv')
+    # ========================
+
+    # ========== WR ==========
+    wr_dfs = []
+    tot_wrs = len(wrs)
+    i = 0
+    for wr in wrs:
+        wr_df = get_player_df(wr)
+        if len(wr_df) >= MIN_GAMES:
+            try:
+                final_df = _wr(wr, wr_df)
+                time.sleep(2)
+                wr_dfs.append(final_df)
+            except Exception:
+                pass
+
+        i += 1
+        print(f'{round(i/tot_wrs, 2)*100}% of WRs complete')
+
+    wr_df = pd.concat(wr_dfs)
+    wr_df.to_csv('results/wrs.csv')
 
 
 def _check_if_in_fant_df(players_dict, fant_df):
@@ -103,26 +179,19 @@ def _qb(qb, df):
                      (df['Int']*PASSING_INT) + (df['Yds.2']*RUSHING_YARDS) +
                      (df['TD.1']*RUSHING_TD))
 
-    # passing_yards_series = df['Yds']
-    # passing_td_series = df['TD']
-    # rushing_yards_series = df['Yds.2']
-    # rushing_td_series = df['TD.1']
-    # passing_int_series = df['Int']
-
     fant_pts_median = df['FantPts'].median()
     fant_pts_mean = df['FantPts'].mean()
     fant_pts_std = df['FantPts'].std()
     fant_pts_30_percentile = np.percentile(df['FantPts'], 30)
     fant_pts_plus_std = fant_pts_mean + fant_pts_std
     fant_pts_minus_std = fant_pts_mean - fant_pts_std
-
-    # print(fant_pts_median, fant_pts_30_percentile)
-    # df.to_csv(f'results/{qb}.csv')
+    fant_pts_skew = fant_pts_median - fant_pts_mean
 
     data = {'Name': [qb], 'FantPts MEDIAN': [fant_pts_median],
-            'FantPts 30 Percentile': [fant_pts_30_percentile],
+            'FantPts SKEW': [fant_pts_skew],
             'FantPts +SD': [fant_pts_plus_std],
-            'FantPts -SD': [fant_pts_minus_std]}
+            'FantPts -SD': [fant_pts_minus_std],
+            'FantPts 30 Percentile': [fant_pts_30_percentile]}
     final_df = pd.DataFrame.from_dict(data=data)
     final_df.set_index('Name', inplace=True)
 
@@ -144,11 +213,13 @@ def _rb(rb, df):
     fant_pts_30_percentile = np.percentile(df['FantPts'], 30)
     fant_pts_plus_std = fant_pts_mean + fant_pts_std
     fant_pts_minus_std = fant_pts_mean - fant_pts_std
+    fant_pts_skew = fant_pts_median - fant_pts_mean
 
     data = {'Name': [rb], 'FantPts MEDIAN': [fant_pts_median],
-            'FantPts 30 Percentile': [fant_pts_30_percentile],
+            'FantPts SKEW': [fant_pts_skew],
             'FantPts +SD': [fant_pts_plus_std],
-            'FantPts -SD': [fant_pts_minus_std]}
+            'FantPts -SD': [fant_pts_minus_std],
+            'FantPts 30 Percentile': [fant_pts_30_percentile]}
     final_df = pd.DataFrame.from_dict(data=data)
     final_df.set_index('Name', inplace=True)
 
@@ -170,99 +241,17 @@ def _wr(wr, df):
     fant_pts_30_percentile = np.percentile(df['FantPts'], 30)
     fant_pts_plus_std = fant_pts_mean + fant_pts_std
     fant_pts_minus_std = fant_pts_mean - fant_pts_std
+    fant_pts_skew = fant_pts_median - fant_pts_mean
 
     data = {'Name': [wr], 'FantPts MEDIAN': [fant_pts_median],
-            'FantPts 30 Percentile': [fant_pts_30_percentile],
+            'FantPts SKEW': [fant_pts_skew],
             'FantPts +SD': [fant_pts_plus_std],
-            'FantPts -SD': [fant_pts_minus_std]}
+            'FantPts -SD': [fant_pts_minus_std],
+            'FantPts 30 Percentile': [fant_pts_30_percentile]}
     final_df = pd.DataFrame.from_dict(data=data)
     final_df.set_index('Name', inplace=True)
 
     return final_df
-
-
-def main():
-    players_dict = Data.players()
-    fant_df = Data.fantasy_df()
-
-    _check_if_in_fant_df(players_dict, fant_df)
-
-    positions_dict = _get_positions(players_dict, fant_df)
-
-    players_dict, dropped = _drop_no_pos_players(players_dict, positions_dict)
-    # players_dict, dropped2 = _drop_few_games_players(players_dict)
-    # print(dropped2)
-    # print(positions_dict['Dalvin Cook'])
-
-    pos_df = pd.DataFrame.from_dict(positions_dict, orient='index')
-
-    main_df = pd.DataFrame.from_dict(players_dict, orient='index')
-
-    main_df = pd.concat([main_df, pos_df], axis=1)
-    main_df.dropna(inplace=True)
-    main_df.columns = ['Link', 'Pos']
-    # main_df.to_csv('testasfad.csv')
-
-    qbs = main_df[main_df['Pos'] == 'QB'].index.tolist()
-    rbs = main_df[main_df['Pos'] == 'RB'].index.tolist()
-    wrs = main_df[main_df['Pos'] == 'WR'].index.tolist()
-
-    # ========== QB ==========
-    qb_dfs = []
-    tot_qbs = len(qbs)
-    i = 0
-    for qb in qbs:
-        qb_df = get_player_df(qb)
-        if len(qb_df) >= MIN_GAMES:
-            final_df = _qb(qb, qb_df)
-            time.sleep(2)
-            qb_dfs.append(final_df)
-
-        i += 1
-        print(f'{round(i/tot_qbs, 2)*100}% of QBs complete')
-
-    qb_df = pd.concat(qb_dfs)
-    qb_df.to_csv('results/qbs.csv')
-    # ========================
-
-    # ========== RB ==========
-    rb_dfs = []
-    tot_rbs = len(rbs)
-    i = 0
-    for rb in rbs:
-        rb_df = get_player_df(rb)
-        if len(rb_df) >= MIN_GAMES:
-            final_df = _rb(rb, rb_df)
-            time.sleep(2)
-            rb_dfs.append(final_df)
-
-        i += 1
-        print(f'{round(i/tot_rbs, 2)*100}% of RBs complete')
-
-    rb_df = pd.concat(rb_dfs)
-    rb_df.to_csv('results/rbs.csv')
-    # ========================
-
-    # ========== WR ==========
-    wr_dfs = []
-    tot_wrs = len(wrs)
-    i = 0
-    for wr in wrs:
-        wr_df = get_player_df(wr)
-        if len(wr_df) >= MIN_GAMES:
-            try:
-                final_df = _wr(wr, wr_df)
-                time.sleep(2)
-                wr_dfs.append(final_df)
-            except Exception:
-                pass
-
-        i += 1
-        print(f'{round(i/tot_wrs, 2)*100}% of WRs complete')
-
-    wr_df = pd.concat(wr_dfs)
-    wr_df.to_csv('results/wrs.csv')
-    # ========================
 
 
 if __name__ == "__main__":
